3dc93a0477485500fd35c53ed45b2ae814c54ccf
Jeff Tratner <jtratner@counsyl.com>
2018-01-09 08:07:07 -0800

    Fix up test completely

--- fire/test_main.py
+++ fire/test_main.py
@@ -4,7 +4,6 @@ Test using fire via python -m fire
 import os
 
 from fire import __main__
-from fire.core import FireExit
 from fire import testutils
 
 class TestAsMainModule(testutils.BaseTestCase):
@@ -12,12 +11,7 @@ class TestAsMainModule(testutils.BaseTestCase):
     # confirm one of the usage lines has tempfile gettempdir
     # with self.assertRaisesFireExit(2, 'tempfile gettempdir'):
     with self.assertOutputMatches('tempfile gettempdir'):
-      try:
-        __main__.main(['__main__.py', 'tempfile'])
-      except FireExit as e:
-        self.assertEqual(e.trace.name, 'tempfile')
-        raise
-      assert False, 'should have raised FireExit'
+      __main__.main(['__main__.py', 'tempfile'])
   def test_arg_passing(self):
     expected = os.path.join('part1', 'part2', 'part3')
     with self.assertOutputMatches('%s\n' % expected):
