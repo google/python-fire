ce7ea6cd35213763f9ba0fd860a3d098cfbbdfe3
Jeff Tratner <jtratner@counsyl.com>
2018-01-09 19:39:59 -0800

    More lint fixes

--- fire/__main__.py
+++ fire/__main__.py
@@ -1,16 +1,18 @@
-"""
-Module for running python fire as a "main" function and thereby allowing using
-fire with other programs.
+"""Run python as a "main" function (i.e., ``python -m fire``)
+
+This allows using fire in other libraries without modifying their code.
 """
 import importlib
 import sys
 
 import fire
 
+
 def main(args):
   module_name = args[1]
   module = importlib.import_module(module_name)
   fire.Fire(module, name=module_name, command=args[2:])
+
 
 if __name__ == '__main__':
   main(sys.argv)
--- fire/test_main.py
+++ fire/test_main.py
@@ -6,12 +6,14 @@ import os
 from fire import __main__
 from fire import testutils
 
-class TestAsMainModule(testutils.BaseTestCase):
-  def test_name_setting(self):
+
+class MainModuleTest(testutils.BaseTestCase):
+  def testNameSetting(self):
     # confirm one of the usage lines has tempfile gettempdir
     with self.assertOutputMatches('tempfile gettempdir'):
       __main__.main(['__main__.py', 'tempfile'])
-  def test_arg_passing(self):
+
+  def testArgPassing(self):
     expected = os.path.join('part1', 'part2', 'part3')
     with self.assertOutputMatches('%s\n' % expected):
       __main__.main(['__main__.py', 'os.path', 'join', 'part1', 'part2',
