105930e3d984c9d5d05fe8406e18eb57bdea32e9
David Bieber <dbieber@google.com>
2019-01-23 12:38:00 -0500

    Remove deprecated hypothesis setting.
    PiperOrigin-RevId: 230545402
    Change-Id: Ib1ead3e70bfec9a7fe1aa803200c307fd5d84375

--- fire/docstrings_fuzz_test.py
+++ fire/docstrings_fuzz_test.py
@@ -29,8 +29,7 @@ from hypothesis import strategies as st
 
 class DocstringsFuzzTest(testutils.BaseTestCase):
 
-  @settings(max_examples=10000,
-            timeout=120)
+  @settings(max_examples=10000)
   @given(st.text(min_size=1))
   @example('This is a one-line docstring.')
   def test_fuzz_parse(self, value):
