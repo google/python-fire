86e2c355517d51488488068cf833185442e91d3c
David Bieber <dbieber@google.com>
2019-07-25 16:39:33 -0700

    Set version to 0.2.0.
    PiperOrigin-RevId: 260046043
    Change-Id: I1f6f28c2b6984cb8645fa1b8f34931fcb0c30e9f

--- fire/__init__.py
+++ fire/__init__.py
@@ -21,4 +21,4 @@ from __future__ import print_function
 from fire.core import Fire
 
 __all__ = ['Fire']
-__version__ = '0.1.4'
+__version__ = '0.2.0'
--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ TEST_DEPENDENCIES = [
     'python-Levenshtein',
 ]
 
-VERSION = '0.1.4'
+VERSION = '0.2.0'
 URL = 'https://github.com/google/python-fire'
 
 setup(
