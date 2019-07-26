a54ef58d16168d1e337a360a6f978072c8514117
David Bieber <dbieber@google.com>
2019-07-26 11:51:36 -0700

    Adds BinaryCanvas as test component.
    PiperOrigin-RevId: 260185289
    Change-Id: I8c8354359486b9f0c63a7c524eb9a904de6c3249

--- fire/test_components.py
+++ fire/test_components.py
@@ -465,3 +465,36 @@ def fn_with_code_in_docstring():
     True.
   """
   return True
+
+
+class BinaryCanvas(object):
+  """A canvas with which to make binary art, one bit at a time."""
+
+  def __init__(self, size=10):
+    self.pixels = [[0] * size for _ in range(size)]
+    self._size = size
+    self._row = 0  # The row of the cursor.
+    self._col = 0  # The column of the cursor.
+
+  def __str__(self):
+    return '\n'.join(
+        ' '.join(str(pixel) for pixel in row) for row in self.pixels)
+
+  def show(self):
+    print(self)
+    return self
+
+  def move(self, row, col):
+    self._row = row % self._size
+    self._col = col % self._size
+    return self
+
+  def on(self):
+    return self.set(1)
+
+  def off(self):
+    return self.set(0)
+
+  def set(self, value):
+    self.pixels[self._row][self._col] = value
+    return self
