36b6d2edafdc9faedb51e3c69e072cea2d26e6bb
uburuntu <bekbulatov.ramzan@ya.ru>
2020-05-25 18:57:26 +0300

    enh: remove outdated travis workaround for 3.7

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
 
         'Operating System :: OS Independent',
         'Operating System :: POSIX',
