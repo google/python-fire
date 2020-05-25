910a92ae1e006150f3008300e0e25dedd00ecfe8
uburuntu <bekbulatov.ramzan@ya.ru>
2020-05-25 18:58:22 +0300

    new: add python 3.8 to travis.yml

--- setup.py
+++ setup.py
@@ -70,7 +70,6 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
 
         'Operating System :: OS Independent',
         'Operating System :: POSIX',
