c39de6ae333fdaf7d3371ab7cb213cb28703120d
MichaelCG8 <42502192+MichaelCG8@users.noreply.github.com>
2021-01-22 08:40:05 -0800

    issue-261 Fixed missing parts of argument descriptions for Google and Numpy style docstrings. 482bb9da4bc09522984d982716a79caeafabf3e2 by Michael Garbutt <michael.c.garbutt@gmail.com>:
    COPYBARA_INTEGRATE_REVIEW=https://github.com/google/python-fire/pull/262 from MichaelCG8:issue-261-bugfix-multi-line-docstring-parameter-descriptions 482bb9da4bc09522984d982716a79caeafabf3e2
    PiperOrigin-RevId: 353248737
    Change-Id: I3033200c69c5aba0ce9bb819574f37248e32c1cb

--- fire/docstrings_test.py
+++ fire/docstrings_test.py
@@ -144,6 +144,32 @@ class DocstringsTest(testutils.BaseTestCase):
     )
     self.assertEqual(expected_docstring_info, docstring_info)
 
+  def test_google_format_multiline_arg_description(self):
+    docstring = """Docstring summary.
+
+    This is a longer description of the docstring. It spans multiple lines, as
+    is allowed.
+
+    Args:
+        param1 (int): The first parameter.
+        param2 (str): The second parameter. This has a lot of text, enough to
+        cover two lines.
+    """
+    docstring_info = docstrings.parse(docstring)
+    expected_docstring_info = DocstringInfo(
+        summary='Docstring summary.',
+        description='This is a longer description of the docstring. It spans '
+        'multiple lines, as\nis allowed.',
+        args=[
+            ArgInfo(name='param1', type='int',
+                    description='The first parameter.'),
+            ArgInfo(name='param2', type='str',
+                    description='The second parameter. This has a lot of text, '
+                                'enough to cover two lines.'),
+        ],
+    )
+    self.assertEqual(expected_docstring_info, docstring_info)
+
   def test_rst_format_typed_args_and_returns(self):
     docstring = """Docstring summary.
 
@@ -204,6 +230,35 @@ class DocstringsTest(testutils.BaseTestCase):
         ],
         # TODO(dbieber): Support return type.
         returns='bool True if successful, False otherwise.',
+    )
+    self.assertEqual(expected_docstring_info, docstring_info)
+
+  def test_numpy_format_multiline_arg_description(self):
+    docstring = """Docstring summary.
+
+    This is a longer description of the docstring. It spans across multiple
+    lines.
+
+    Parameters
+    ----------
+    param1 : int
+        The first parameter.
+    param2 : str
+        The second parameter. This has a lot of text, enough to cover two
+        lines.
+    """
+    docstring_info = docstrings.parse(docstring)
+    expected_docstring_info = DocstringInfo(
+        summary='Docstring summary.',
+        description='This is a longer description of the docstring. It spans '
+        'across multiple\nlines.',
+        args=[
+            ArgInfo(name='param1', type='int',
+                    description='The first parameter.'),
+            ArgInfo(name='param2', type='str',
+                    description='The second parameter. This has a lot of text, '
+                                'enough to cover two lines.'),
+        ],
     )
     self.assertEqual(expected_docstring_info, docstring_info)
 
