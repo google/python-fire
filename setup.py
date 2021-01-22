f2ab298e436f12d6a3fe36744b7eab6afcd04d80
David Bieber <dbieber@google.com>
2021-01-22 08:57:47 -0800

    Test Python Fire on Python 3.9
    PiperOrigin-RevId: 353251383
    Change-Id: Iaa74341995640e59619f922997bbc451003a0f1c

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
 
         'Operating System :: OS Independent',
         'Operating System :: POSIX',
