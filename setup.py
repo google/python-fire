f4b7553a19a19b4013cdba6e8ee17f7e93c069ff
Arnav Bansal <70932975+arnavbansal2764@users.noreply.github.com>
2021-09-30 22:40:56 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# This is an amazing project
 """The setup.py file for Python Fire."""
 
 from setuptools import setup
