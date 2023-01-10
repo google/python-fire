f1ebc50816ab3e164db90e90b64eae88135d0606
Jirka <jirka.borovec@seznam.cz>
2023-01-10 10:51:32 +0100

    adding python 3.10

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
 
         'Operating System :: OS Independent',
         'Operating System :: POSIX',
