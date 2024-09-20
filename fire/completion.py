b3e174828324832ab738567a4ac784a0cf33545d
David Bieber <david810@gmail.com>
2024-09-19 20:08:31 -0400

    Keep future imports for use in MemberVisible

--- fire/completion.py
+++ fire/completion.py
@@ -14,6 +14,10 @@
 
 """Provides tab completion functionality for CLIs built with Fire."""
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import collections
 import copy
 import inspect
